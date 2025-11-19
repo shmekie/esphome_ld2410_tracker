d121b16a0d6171d33bac873900e3207a9bc45860
shmekie <119074792+shmekie@users.noreply.github.com>
2025-11-19 18:02:55 -0500

    Update ld2410_tracker.h

--- components/ld2410_tracker/ld2410_tracker.h
+++ components/ld2410_tracker/ld2410_tracker.h
@@ -1,1 +1,22 @@
+#pragma once
+#include "esphome.h"
+#include <vector>
+
+class LD2410Tracker : public esphome::Component {
+ public:
+  LD2410Tracker() : uart_(nullptr) {}
+
+  void set_uart(esphome::uart::UARTComponent *uart) { uart_ = uart; }
+
+  void setup() override {}
+  void loop() override;
+
+ protected:
+  esphome::uart::UARTComponent *uart_;
+
+  bool parse_frame(std::vector<uint8_t> &stream,
+                   bool &detected,
+                   float &angle_deg,
+                   float &dist_cm);
+};
 
