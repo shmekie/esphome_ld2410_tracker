5191c29ec6ae5dcab428ef3b969574e1768acb80
shmekie <119074792+shmekie@users.noreply.github.com>
2025-11-20 18:17:25 -0500

    Update ld2410_tracker.h

--- components/ld2410_tracker/ld2410_tracker.h
+++ components/ld2410_tracker/ld2410_tracker.h
@@ -1,18 +1,26 @@
 #pragma once
 #include "esphome.h"
 #include <vector>
+#include <functional>
 
 class LD2410Tracker : public esphome::Component {
  public:
-  LD2410Tracker() : uart_(nullptr) {}
-
   void set_uart(esphome::uart::UARTComponent *uart) { uart_ = uart; }
 
-  void setup() override {}
+  void set_update_object_present(std::function<void(bool)> f) { update_object_present_ = f; }
+  void set_update_angle(std::function<void(float)> f) { update_angle_ = f; }
+  void set_update_distance(std::function<void(float)> f) { update_distance_ = f; }
+  void set_move_stepper(std::function<void(float)> f) { move_stepper_ = f; }
+
   void loop() override;
 
  protected:
-  esphome::uart::UARTComponent *uart_;
+  esphome::uart::UARTComponent *uart_{nullptr};
+
+  std::function<void(bool)> update_object_present_;
+  std::function<void(float)> update_angle_;
+  std::function<void(float)> update_distance_;
+  std::function<void(float)> move_stepper_;
 
   bool parse_frame(std::vector<uint8_t> &stream,
                    bool &detected,
